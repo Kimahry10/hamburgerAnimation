4a83b96696f00edcf001920114c01ff4b62d84ed
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-11-19 22:40:55 +0100

    added menu toggle

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open');
+    nav.classList.add('show');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    nav.classList.remove('show');
     menuOpen = false;
   }
 });
